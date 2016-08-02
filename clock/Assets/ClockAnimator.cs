d48879244645360284c916fc6e7afacf6d49b498
Colin Parsons <casparsons@gmail.com>
2016-08-02 14:58:44 -0700

    add clock and fractal projects

---
+++ clock/Assets/ClockAnimator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System;
+
+public class ClockAnimator : MonoBehaviour {
+
+	public Transform hours, minutes, seconds;
+
+	private const float
+		hoursToDegrees = 360f / 12f,
+		minutesToDegrees = 360f / 60f,
+		secondsToDegrees = 360f / 60f;
+
+	
+	// Update is called once per frame
+	void Update () {
+		TimeSpan time = DateTime.Now.TimeOfDay;
+		hours.localRotation = 
+			Quaternion.Euler (0f, 0f, (float)time.TotalHours * -hoursToDegrees);
+		minutes.localRotation = 
+			Quaternion.Euler (0f, 0f, (float)time.TotalMinutes * -minutesToDegrees);
+		seconds.localRotation = 
+			Quaternion.Euler (0f, 0f, (float)time.TotalSeconds * -secondsToDegrees);
+
+	}
+}
