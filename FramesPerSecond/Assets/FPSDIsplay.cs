7151d7a2939e1b0d75e93d69d2a043464ce3d2fa
Colin Parsons <casparsons@gmail.com>
2016-08-08 14:16:42 -0700

    add FramesPerSecond exercise

---
+++ FramesPerSecond/Assets/FPSDIsplay.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(FPSCounter))]
+public class FPSDIsplay : MonoBehaviour {
+
+	public Text highestFPSLabel, averageFPSLabel, lowestFPSLabel;
+
+	FPSCounter fpsCounter;
+
+	void Awake () {
+		fpsCounter = GetComponent<FPSCounter> ();
+	}
+
+	void Update () {
+		highestFPSLabel.text = Mathf.Clamp(fpsCounter.HighestFPS, 0, 99).ToString ();
+		averageFPSLabel.text = Mathf.Clamp(fpsCounter.AverageFPS, 0, 99).ToString ();
+		lowestFPSLabel.text = Mathf.Clamp(fpsCounter.LowestFPS, 0, 99).ToString ();
+	}
+
+}
